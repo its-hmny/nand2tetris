fa552d5febfb15a9e98815d6fb7bcd6371d7e1ce
Enea Guidi <guidienea@gmail.com>
2023-12-18 23:19:12 +0000

    feat(projects): Added Mult.asm and  Mult.hack (compiled)
    The test suite has run and the output checks out w/ the expected one.

--- projects/04 - Machine Language/01 - Mult/Mult.asm
+++ projects/04 - Machine Language/01 - Mult/Mult.asm
@@ -7,4 +7,29 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// Prelude: cleans the R2 register to avoid memory corruption
+@R2
+M = 0
+
+(LOOP)
+    // If (R1 == 0) goto EXIT
+    @R1
+    D = M 
+    @EXIT 
+    D; JEQ 
+    // R2 = R2 + R0
+    @R0
+    D = M
+    @R2
+    M = M + D 
+    // R1 = R1 - 1
+    @R1
+    M = M -1
+    // goto LOOP (restart iteration)
+    @LOOP
+    0; JMP
+
+
+(EXIT) // Nothing more after this, endless cycle
+    @EXIT
+    0; JMP
