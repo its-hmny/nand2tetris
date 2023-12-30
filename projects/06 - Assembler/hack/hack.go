f447345757293cbcb990210db27cbaab8037f60d
Enea Guidi <guidienea@gmail.com>
2023-12-30 23:59:00 +0000

    chore(assembler): Added CInstruction struct + bitcode mapping
    The current implementation for the C Instruction in the Hack
    architecture is quite simple , we just have the 3 different section
    each mapped directly to their bits using Go map(s). At this point
    the codegen process is a simple get operation on the map plus some
    bitwise logic to pack each bitcode in its rightful place.

--- projects/06 - Assembler/hack/hack.go
+++ projects/06 - Assembler/hack/hack.go
@@ -48,11 +48,33 @@ type AInstruction struct {
 //										C Instructions
 // ======================================================================================
 
+var (
+	OpTable = map[string]uint16{
+		"0": 0b0101010, "1": 0b011111, "-1": 0b0111010,
+		"D": 0b0001100, "A": 0b0110000, "M": 0b1110000,
+		"!D": 0b0001101, "!A": 0b0110001, "!M": 0b1110001,
+		"-D": 0b0001111, "-A": 0b0110011, "-M": 0b1110011,
+		"D+1": 0b0011111, "A+1": 0110111, "M+1": 0b1110111,
+		"D-1": 0b0001110, "A-1": 0b0110010, "M-1": 0b1110010,
+		"D+A": 0b0000010, "D+M": 0b1000010,
+		"D-A": 0b0010011, "D-M": 0b1010011, "A-D": 0b0000111, "M-D": 0b1000111,
+		"D&A": 0b0000000, "D&M": 0b100000, "D|A": 0b0010101, "D|M": 0b1010101,
+	}
+
+	DestTable = map[string]uint16{
+		"M": 0b001, "D": 0b010, "A": 0b100,
+		"MD": 0b011, "AM": 0b101, "AD": 0b110, "AMD": 0b111,
+	}
+
+	JumpTable = map[string]uint16{
+		"JGT": 0b001, "JEQ": 0b010, "JGE": 0b011,
+		"JLT": 0b100, "JNE": 0b101, "JLE": 0b110, "JMP": 0b111,
+	}
+)
+
 // In memory representation of a C instruction for the Hack computer specification
 type CInstruction struct {
-	// TODO (hmny): Populate this struct w/ the required fields
+	Operation   string // The calculation bit-code to be performed by the ALU
+	Destination string // The destination registry where the result will be saved
+	Jump        string // The jump directive to interrupt the linear flow execution
 }
-
-
-
-
