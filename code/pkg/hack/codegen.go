340ff31db4e689acab635031a06134c839fe0496
Enea Guidi <50141843+its-hmny@users.noreply.github.com>
2024-01-18 17:43:01 +0000

    chore(hack): Moved translation tables to 'codegen.go'

--- code/pkg/hack/codegen.go
+++ code/pkg/hack/codegen.go
@@ -6,6 +6,60 @@ import (
 )
 
 // ----------------------------------------------------------------------------
+// Translation tables
+
+// This section contains the translation tables cornerstone of the codegen phase.
+//
+// This table provides a simple yet effective way to resolve the everything built-in and
+// in the Hack specification. Notably we have a the following tables defined:
+//	- 'BuiltInTable': Specifies how to translate BuiltIn labels in A instructions to their address
+//  - 'CompTable': Specifies how to translate the 'Comp' opcode in C instructions
+//  - 'DestTable': Specifies how to translate the 'Dest' opcode in C instructions
+//  - 'JumpTable': Specifies how to translate the 'Jump' opcode in C instructions
+
+var (
+	BuiltInTable = map[string]uint16{
+		// Virtual Machine specific aliases (see project 7)
+		"SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4,
+		// Named general purpose registers
+		"R0": 0, "R1": 1, "R2": 2, "R3": 3, "R4": 4, "R5": 5,
+		"R6": 6, "R7": 7, "R8": 8, "R9": 9, "R10": 10, "R11": 11,
+		"R12": 12, "R13": 13, "R14": 14, "R15": 15,
+		// Memory mapped I/O locations
+		"SCREEN": 16384, "KBD": 24576,
+	}
+
+	CompTable = map[string]uint16{
+		// - Constants and identities
+		"0": 0b0101010, "1": 0b0111111, "-1": 0b0111010,
+		"D": 0b0001100, "A": 0b0110000, "M": 0b1110000,
+		// - Binary and numerical negations
+		"!D": 0b0001101, "!A": 0b0110001, "!M": 0b1110001,
+		"-D": 0b0001111, "-A": 0b0110011, "-M": 0b1110011,
+		// - Increment and decrement operations
+		"D+1": 0b0011111, "A+1": 0b0110111, "M+1": 0b1110111,
+		"D-1": 0b0001110, "A-1": 0b0110010, "M-1": 0b1110010,
+		// - Register with register operations
+		"D+A": 0b0000010, "D+M": 0b1000010,
+		"D-A": 0b0010011, "D-M": 0b1010011,
+		"A-D": 0b0000111, "M-D": 0b1000111,
+		// - Bitwise register with register operations
+		"D&A": 0b0000000, "D&M": 0b1000000,
+		"D|A": 0b0010101, "D|M": 0b1010101,
+	}
+
+	DestTable = map[string]uint16{
+		"": 0b000, "M": 0b001, "D": 0b010, "A": 0b100,
+		"MD": 0b011, "AM": 0b101, "AD": 0b110, "AMD": 0b111,
+	}
+
+	JumpTable = map[string]uint16{
+		"": 0b000, "JGT": 0b001, "JEQ": 0b010, "JGE": 0b011,
+		"JLT": 0b100, "JNE": 0b101, "JLE": 0b110, "JMP": 0b111,
+	}
+)
+
+// ----------------------------------------------------------------------------
 // Code Generator
 
 // Takes some a set of 'hack.Instruction' and spits out their binary counterparts.
--- code/pkg/hack/hack.go
+++ code/pkg/hack/hack.go
@@ -7,58 +7,12 @@ package hack
 //
 // We declare a shared 'Instruction' interface for both A and C instructions as well
 // as defining some useful constants for runtime assertions during the codegen phase
-// such as the 'MaxAddressableMemory' that defines the upper limit to Memory capacity
-// and the 'BuiltInTable' that defines the hardcoded symbols for some well-known registers.
+// such as the 'MaxAddressableMemory' that defines the upper limit to Memory capacity.
 
 // Just used to put together A and C instructions struct, use type switch to disambiguate.
 type Instruction interface{}
 
-const MaxAddressableMemory uint16 = (1 << 15) // Max memory location available from an A instruction.
-
-var BuiltInTable = map[string]uint16{ // Predefined/Built-In symbols by the Hack instruction set.
-	// Virtual Machine specific aliases (see project 7)
-	"SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4,
-	// Named general purpose registers
-	"R0": 0, "R1": 1, "R2": 2, "R3": 3, "R4": 4, "R5": 5,
-	"R6": 6, "R7": 7, "R8": 8, "R9": 9, "R10": 10, "R11": 11,
-	"R12": 12, "R13": 13, "R14": 14, "R15": 15,
-	// Memory mapped I/O locations
-	"SCREEN": 16384, "KBD": 24576,
-}
-
-// Also defined here we can find three direct translation tables, these are purposefully
-// built to simplify the conversion from textual to binary of C instructions during the
-// codegen phase. As one can see there's a mapping table for each part of a C instruction.
-var (
-	CompTable = map[string]uint16{
-		// - Constants and identities
-		"0": 0b0101010, "1": 0b0111111, "-1": 0b0111010,
-		"D": 0b0001100, "A": 0b0110000, "M": 0b1110000,
-		// - Binary and numerical negations
-		"!D": 0b0001101, "!A": 0b0110001, "!M": 0b1110001,
-		"-D": 0b0001111, "-A": 0b0110011, "-M": 0b1110011,
-		// - Increment and decrement operations
-		"D+1": 0b0011111, "A+1": 0b0110111, "M+1": 0b1110111,
-		"D-1": 0b0001110, "A-1": 0b0110010, "M-1": 0b1110010,
-		// - Register with register operations
-		"D+A": 0b0000010, "D+M": 0b1000010,
-		"D-A": 0b0010011, "D-M": 0b1010011,
-		"A-D": 0b0000111, "M-D": 0b1000111,
-		// - Bitwise register with register operations
-		"D&A": 0b0000000, "D&M": 0b1000000,
-		"D|A": 0b0010101, "D|M": 0b1010101,
-	}
-
-	DestTable = map[string]uint16{
-		"": 0b000, "M": 0b001, "D": 0b010, "A": 0b100,
-		"MD": 0b011, "AM": 0b101, "AD": 0b110, "AMD": 0b111,
-	}
-
-	JumpTable = map[string]uint16{
-		"": 0b000, "JGT": 0b001, "JEQ": 0b010, "JGE": 0b011,
-		"JLT": 0b100, "JNE": 0b101, "JLE": 0b110, "JMP": 0b111,
-	}
-)
+const MaxAddressableMemory uint16 = (1 << 15) // Max memory address indexable for an A Instruction.
 
 // ----------------------------------------------------------------------------
 // A Instructions
@@ -66,15 +20,15 @@ var (
 // In memory representation of an A Instruction for the Hack architecture spec.
 //
 // The A instruction has only one functionality in the Hack computer, it instructs
-// the CPU to load a specific memory address/location from the computer memory (this
-// includes both the RAM and the memory mapped I/O).
+// the CPU to load a specific memory address from the computer memory (this includes
+// both the RAM as well as the memory mapped I/O such as Keyboard and Screen).
 //
 // The location can be expressed in multiple way:
-// - A raw memory address (e;g; 1, 2, 3)
-// - Specified through a user defined label (e;g; LOOP, ADD, TEMP)
-// - Using one of the built-in symbols of the Hack architecture (e;g; SP, THIS, THAT)
+// - A raw memory address (e.g. 1, 2, 3)
+// - A user defined label (e.g. LOOP, ADD, TEMP)
+// - A built-in symbols from the Hack architecture spec (e.g. SP, THIS, THAT)
 type AInstruction struct {
-	LocType LocationType // The subtype of the location identified by 'Name'
+	LocType LocationType // The type of the location identified by 'Name' field
 	LocName string       // A generic "payload" (the label/builtin/raw symbol)
 }
 
@@ -96,9 +50,9 @@ const (
 // specify jump conditions to change the execution flow at runtime.
 //
 // The location can be expressed in multiple way:
-// - A raw memory address (e;g; 1, 2, 3)
-// - Specified through a user defined label (e;g; LOOP, ADD, TEMP)
-// - Using one of the built-in symbols of the Hack architecture (e;g; SP, THIS, THAT)
+// - A raw memory address (e.g. 1, 2, 3)
+// - Specified through a user defined label (e.g. LOOP, ADD, TEMP)
+// - Using one of the built-in symbols of the Hack architecture (e.g. SP, THIS, THAT)
 type CInstruction struct {
 	Comp string // The 'computation' bit-codes, defines the calculation that the CPU should perform
 	Dest string // The 'destination' bit-codes, defines if/where the result should be saved
