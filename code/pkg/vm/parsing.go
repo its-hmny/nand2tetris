9b989f0e099c7d6be60c8997ba0919ab10368195
Enea Guidi <guidienea@gmail.com>
2024-01-29 22:25:06 +0000

    feat(vm): Completed VM parsing w/ missing statements
    The following commits adds (untested) support for the remaining syntax
    constructs of the Virtual Machine language, In particular the following
    have been added:
      - Unconditional & conditional jumping
      - Function and label declarations
      - Function call and return statements

--- code/pkg/vm/parsing.go
+++ code/pkg/vm/parsing.go
@@ -16,10 +16,15 @@ var (
 	// each contains the bytecode for the specific module/class (a separate translation unit).
 	pModule = ast.ManyUntil("module", nil, ast.OrdChoice("node", nil, pComment, pStatement), pc.End())
 
-	// Parser combinator for a generic VM statement (MemoryOp, ArithmeticOp, ...)
-	pStatement = ast.OrdChoice("statement", nil, pMemoryOp, pArithmeticOp)
 	// Parser combinator for comments in Assembler program
 	pComment = ast.And("comment", nil, pc.Atom("//", "//"), pc.Token(`(?m).*$`, "COMMENT"))
+	// Parser combinator for a generic VM statement (MemoryOp, ArithmeticOp, ...)
+	pStatement = ast.OrdChoice("statement", nil,
+		pMemoryOp, pArithmeticOp,
+		pUncondJumpOp, pCondJumpOp,
+		pLabelDecl, pFuncDecl,
+		pReturnOp, pFunCallOp,
+	)
 
 	// Memory operation, compliant with the following syntax: "{push|pop} {segment} {index}"
 	pMemoryOp = ast.And("memory_op", nil, pMemOpType, pSegment, pc.Int())
@@ -32,9 +37,26 @@ var (
 		// Bit-a-bit operations available on the VM bytecode
 		pc.Atom("not", "NOT"), pc.Atom("and", "AND"), pc.Atom("or", "OR"),
 	)
+
+	// Unconditional jump operation, compliant with the following syntax: "goto {symbol}"
+	pUncondJumpOp = ast.And("goto_op", nil, pc.Atom("goto", "GOTO"), pLabel)
+	// Conditional jump operation, compliant with the following syntax: "if goto {symbol}"
+	pCondJumpOp = ast.And("if-goto_op", nil, pc.Atom("if-goto", "IF-GOTO"), pLabel)
+
+	// Label declaration, compliant with the following syntax: "label {symbol}"s
+	pLabelDecl = ast.And("label_decl", nil, pc.Atom("label", "LABEL"), pLabel)
+	// Function declaration, compliant with the following syntax: "function {name} {n_args}"
+	pFuncDecl = ast.And("func_decl", nil, pc.Atom("function", "FUNC"), pFuncName, pc.Int())
+
+	// Return operation, compliant with the following syntax: "return"
+	pReturnOp = ast.And("returns", nil, pc.Atom("return", "RETURN"))
+	// Function calling operation, compliant with the following syntax: "call {name} {n_args}"
+	pFunCallOp = ast.And("func_call", nil, pc.Atom("call", "CALL"), pFuncName, pc.Int())
 )
 
 var (
+	pLabel     = pc.Ident()
+	pFuncName  = pc.Ident()
 	pMemOpType = ast.OrdChoice("mem_op_type", nil, pc.Atom("push", "PUSH"), pc.Atom("pop", "POP"))
 	pSegment   = ast.OrdChoice("mem_segment", nil,
 		pc.Atom("argument", "ARGUMENT"), pc.Atom("local", "LOCAL"),
