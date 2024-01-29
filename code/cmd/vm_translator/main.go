c75d87b3fe879666248b115054d904714b2b3b98
Enea Guidi <guidienea@gmail.com>
2024-01-29 21:56:01 +0000

    feat(vm): Added PCs for VMs MemoryOp and ArithmeticOp statement
    The following commit setups the parsing module for the parsing of the VM
    intermediate language (project 7 and 8). Mainly here we implemented the
    first two basic instruction to load operands from memory and perform
    operations on the stack.
    
    I'm still trying to figure out a better solution than extracting the
    data about the AST by visiting the tree of pc.Queryable which requires
    to check quite pedantically every node before extracting the info.

--- code/cmd/vm_translator/main.go
+++ code/cmd/vm_translator/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
 	"its-hmny.dev/nand2tetris/pkg/asm"
+	"its-hmny.dev/nand2tetris/pkg/vm"
 )
 
 var program = []asm.Statement{
@@ -14,17 +16,36 @@ var program = []asm.Statement{
 	asm.CInstruction{Comp: "D", Jump: "JNE"},
 }
 
+var VmProgram = `
+	push constant 7
+	push constant 8 // Test comment, should work
+	add
+	// Another comment that should work as well
+	push constant 3
+	sub
+`
+
 func main() {
-	// Now, instantiates a code generator for the Asm language
-	codegen := asm.NewCodeGenerator(program)
-	// Iterates over each program instruction and spits out the textual code
-	compiled, err := codegen.Generate()
-	if err != nil {
-		fmt.Printf("ERROR: Unable to complete 'codegen' pass:\n\t %s", err)
+
+	// Instantiate a parser for the vm program
+	parser := vm.NewParser()
+	// Parses the input file content and extract an AST from it
+	_, success := parser.Parse(bytes.NewReader([]byte(VmProgram)))
+	if !success {
+		fmt.Print("ERROR: Unable to complete 'parsing' pass\n")
 		os.Exit(-1)
 	}
 
-	for _, comp := range compiled {
-		fmt.Printf("%s\n", comp)
-	}
+	// // Now, instantiates a code generator for the Asm language
+	// codegen := asm.NewCodeGenerator(program)
+	// // Iterates over each program instruction and spits out the textual code
+	// compiled, err := codegen.Generate()
+	// if err != nil {
+	// 	fmt.Printf("ERROR: Unable to complete 'codegen' pass:\n\t %s", err)
+	// 	os.Exit(-1)
+	// }
+
+	// for _, comp := range compiled {
+	// 	fmt.Printf("%s\n", comp)
+	// }
 }
---
+++ code/pkg/vm/parsing.go
@@ -0,0 +1,80 @@
+package vm
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	pc "github.com/prataprc/goparsec"
+)
+
+var ast = pc.NewAST("virtual_machine", 0)
+
+var (
+	// Parser combinator for a VM module/class, in the nand2tetris VM there's a Java like
+	// behavior where a program is composed of multiple '.vm' file ('.class' in Java) where
+	// each contains the bytecode for the specific module/class (a separate translation unit).
+	pModule = ast.ManyUntil("module", nil, ast.OrdChoice("node", nil, pComment, pStatement), pc.End())
+
+	// Parser combinator for a generic VM statement (MemoryOp, ArithmeticOp, ...)
+	pStatement = ast.OrdChoice("statement", nil, pMemoryOp, pArithmeticOp)
+	// Parser combinator for comments in Assembler program
+	pComment = ast.And("comment", nil, pc.Atom("//", "//"), pc.Token(`(?m).*$`, "COMMENT"))
+
+	// Memory operation, compliant with the following syntax: "{push|pop} {segment} {index}"
+	pMemoryOp = ast.And("memory_op", nil, pMemOpType, pSegment, pc.Int())
+	// Arithmetic operation, could either be binary or unary (modifies only the Stack Pointer)
+	pArithmeticOp = ast.OrdChoice("arithmetic_op", nil,
+		// Comparison operations available on the VM bytecode
+		pc.Atom("eq", "EQ"), pc.Atom("gt", "GT"), pc.Atom("lt", "LT"),
+		// Arithmetic operations available on the VM bytecode
+		pc.Atom("add", "ADD"), pc.Atom("sub", "SUB"), pc.Atom("neg", "NEG"),
+		// Bit-a-bit operations available on the VM bytecode
+		pc.Atom("not", "NOT"), pc.Atom("and", "AND"), pc.Atom("or", "OR"),
+	)
+)
+
+var (
+	pMemOpType = ast.OrdChoice("mem_op_type", nil, pc.Atom("push", "PUSH"), pc.Atom("pop", "POP"))
+	pSegment   = ast.OrdChoice("mem_segment", nil,
+		pc.Atom("argument", "ARGUMENT"), pc.Atom("local", "LOCAL"),
+		pc.Atom("static", "STATIC"), pc.Atom("constant", "CONSTANT"),
+		pc.Atom("this", "THIS"), pc.Atom("that", "THAT"),
+		pc.Atom("temp", "TEMP"), pc.Atom("pointer", "POINTER"),
+	)
+)
+
+type Parser struct{}
+
+func NewParser() Parser { return Parser{} }
+
+func (p *Parser) Parse(r io.Reader) (pc.Queryable, bool) {
+	content, err := io.ReadAll(r)
+	if err != nil {
+		return nil, false
+	}
+
+	// Feature flag: Enable 'goparsec' library's debug logs
+	if os.Getenv("PARSEC_DEBUG") != "" {
+		ast.SetDebug()
+	}
+
+	// We generate the traversable Abstract Syntax Tree from the source content
+	root, _ := ast.Parsewith(pModule, pc.NewScanner(content))
+
+	// Feature flag: Enables export of the AST as Dot file (debug.ast.fot)
+	if os.Getenv("EXPORT_AST") != "" {
+		file, _ := os.Create(fmt.Sprintf("%s/debug.ast.dot", os.Getenv("DEBUG_FOLDER")))
+		defer file.Close()
+
+		file.Write([]byte(ast.Dotstring("\"VM AST\"")))
+	}
+
+	// Feature flag: Enables pretty printing of the AST on the console
+	if os.Getenv("PRINT_AST") != "" {
+		ast.Prettyprint()
+	}
+
+	// TODO (hmny): This hardcoding to true should be changed
+	return root, true // Success is based on the reaching of 'EOF'
+}
