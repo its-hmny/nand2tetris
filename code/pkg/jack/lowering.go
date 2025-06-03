c6099d3f00e5b0f13d26ee49eef6371428ab8659
Enea Guidi <guidienea@gmail.com>
2025-06-03 20:32:41 +0000

    feat(jack): Added Subroutine lowering for 'jack.Class'

--- code/pkg/jack/lowering.go
+++ code/pkg/jack/lowering.go
@@ -35,14 +35,61 @@ func (l *Lowerer) Lowerer() (vm.Program, error) {
 	}
 
 	for name, class := range l.program {
+		operations := []vm.Operation{}
+
 		for _, field := range class.Fields {
-			fmt.Printf("Processing field '%s' for class '%s'\n", field.Name, name)
+			ops, err := l.HandleFieldDecl(field)
+			if err != nil {
+				return nil, fmt.Errorf("error handling field declaration in class '%s': %w", name, err)
+			}
+			operations = append(operations, ops...)
 		}
 
 		for _, subroutine := range class.Subroutines {
-			fmt.Printf("Processing subroutine '%s' for class '%s'\n", subroutine.Name, name)
+			ops, err := l.HandleSubroutine(subroutine)
+			if err != nil {
+				return nil, fmt.Errorf("error handling subroutine '%s' in class '%s': %w", subroutine.Name, name, err)
+			}
+			operations = append(operations, ops...)
 		}
+
+		program[name] = vm.Module(operations)
 	}
 
 	return program, nil
 }
+
+// Specialized function to convert a 'jack.FieldDecl' node to a list of 'vm.Operation'.
+func (l *Lowerer) HandleFieldDecl(field Variable) ([]vm.Operation, error) {
+	return nil, fmt.Errorf("not implemented yet")
+}
+
+// Specialized function to convert a 'jack.Subroutine' node to a list of 'vm.Operation'.
+func (l *Lowerer) HandleSubroutine(routine Subroutine) ([]vm.Operation, error) {
+	localVars := map[string]bool{}
+	for _, stmt := range routine.Statements {
+		// For multiple var declarations we register each new variable in the map/set
+		if varStmt, isVarStmt := stmt.(VarStmt); isVarStmt {
+			for _, variable := range varStmt.Vars {
+				localVars[variable.Name] = true
+			}
+		}
+		// For let declarations we register the new variable in the map/set
+		if letStmt, isLetStmt := stmt.(LetStmt); isLetStmt {
+			// ! Here we support only VarExpr because ArrayExpr would mean that the array has been
+			// ! already declared either in a previous VarStmt or LetStmt, making it redundant.
+			localVars[letStmt.Lhs.(VarExpr).Var] = true
+		}
+	}
+
+	fDecl, fBody := vm.FuncDecl{Name: routine.Name, NLocal: uint8(len(localVars))}, []vm.Operation{}
+
+	for _, stmt := range routine.Statements {
+	}
+
+	return append([]vm.Operation{fDecl}, fBody...), nil
+}
+
+	return append([]vm.Operation{fDecl}, fBody...), nil
+}
+
